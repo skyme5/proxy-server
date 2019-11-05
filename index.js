94bab13ca7f40618b06b0ce4779b1f4cba0f4d28
Aakash Gajjar <skyqutip@gmail.com>
2019-11-05 20:54:41 +0530

    fix name collision

--- index.js
+++ index.js
@@ -2,32 +2,32 @@
  * @Author: Aakash Gajjar
  * @Date:   2019-11-05 19:16:28
  * @Last Modified by:   Sky
- * @Last Modified time: 2019-11-05 20:53:12
+ * @Last Modified time: 2019-11-05 20:54:22
  */
 
 const
     socks5 = require('simple-socks'),
-    server = socks5.createServer().listen(1080),
+    proxy = socks5.createServer().listen(1080),
     http = require('http');
 
 // When a reqest arrives for a remote destination
-server.on('proxyConnect', function (info, destination) {
-    console.log('connected to remote server at %s:%d', info);
+proxy.on('proxyConnect', function (info, destination) {
+    console.log('connected to remote proxy at %s:%d', info);
 });
 
 // When data arrives from the remote connection
-// server.on('proxyData', function (data) {
+// proxy.on('proxyData', function (data) {
 //   console.log(data.length);
 // });
 
 // When an error occurs connecting to remote destination
-server.on('proxyError', function (err) {
-    console.error('unable to connect to remote server');
+proxy.on('proxyError', function (err) {
+    console.error('unable to connect to remote proxy');
     console.error(err);
 });
 
 // When a proxy connection ends
-server.on('proxyEnd', function (response, args) {
+proxy.on('proxyEnd', function (response, args) {
     console.log('socket closed with code %d', response);
     console.log(args);
 });
